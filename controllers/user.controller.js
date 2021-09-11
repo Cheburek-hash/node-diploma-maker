7a2dddfa05c3ff3c1563a67cc9d0e0c81951876e
egor <grebnevegor2000@mail.ru>
2021-09-11 17:13:13 +0300

    user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,44 @@
-const db = require('../server/db');
+const { errors } = require('../models/User');
+const User = require('../models/User');
+
 
 class UserController {
-
+    
     async createUser(req, res){
-        const {name, email, password} = req.body;
+        try {
+ 
+            const {name, email, password} = req.body;
+            if (!name||!email||!password){
+                
+                    User.errors.push({error : {message: "Fields cannot be empty!"}})
+            }
+            else if (password.length < 6){
+                User.errors.push({error : {message: "Password should contain at least 6 symbols"}})
+            }
+            else if (await User.isUserExists(email)){
+                User.errors.push({error : {message: "Such user already exists! "}})
+            }
+           
+            else if (User.errors.length === 0){
+                res.status(200).json(await User.create(name, email, password));
+            }
+                            
+            res.status(400).json(User.errors.shift())
 
-        res.send(
-            await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, 
-            [name, email, password]
-        ));
+
+        } catch (e){
+            res.status(500).json({message: "Something went wrong, try one more time"})
+        }
+
+        
     }
+    
+    async loginUser(req, res){
+
+    }
+    
     async getUsers(req, res){
-        res.send(
-            await db.query(`SELECT * FROM users`)
-        );
+        res.send( await User.getAll());
     }
     async getOneUser(req, res){
         res.send(
