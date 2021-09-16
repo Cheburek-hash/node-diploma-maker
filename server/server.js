0abe0f9b637fc675006ec709a570a1c5eec3486d
egor <grebnevegor2000@mail.ru>
2021-09-16 12:20:08 +0300

    minor server update

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const userRouter = require('../routes/user.routes');
-const authRouter = require('../routes/auth.routes');
-const cors = require('cors');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRouter = require("../routes/user.routes");
+const authRouter = require("../routes/auth.routes");
+const cors = require("cors");
 
 class Server {
-  constructor(){
+  constructor() {
     this.PORT = process.env.PORT || 5000;
-    this.ROOT = require('path').dirname(require.main.filename);
+    this.ROOT = require("path").dirname(require.main.filename);
     this.app = express();
-    
-
-   }
+  }
   start = () => {
     this.app.listen(this.PORT, () => {
-      console.log(`App listening at http://localhost:${this.PORT}`)
+      console.log(`App listening at http://localhost:${this.PORT}`);
     });
     this.app.use(bodyParser.json());
     this.app.use(cors());
-    this.app.use('/api', userRouter);
-    this.app.use('/api/auth', authRouter)
-        
-  }
+    this.app.use("/api", userRouter);
+    this.app.use("/api", authRouter);
+  };
 }
-
 
 module.exports = Server;
