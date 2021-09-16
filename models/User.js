b998bbabc6690cca2ed358c73e712b5bf5f6e443
egor <grebnevegor2000@mail.ru>
2021-09-16 12:21:30 +0300

    user model formating

--- models/User.js
+++ models/User.js
@@ -1,30 +1,31 @@
-const db = require('../server/db');
+const db = require("../server/db");
 
 class User {
+  async isUserExists(email) {
+    return (
+      await db.query(`SELECT EXISTS(SELECT 1 FROM users WHERE email = $1);`, [
+        email,
+      ])
+    ).rows[0].exists;
+  }
+
+  async findOne(email) {
+    return (await db.query(`SELECT * FROM users WHERE email = $1;`, [email]))
+      .rows[0];
+  }
+
   
-    async isUserExists(email){
-        return (await db.query(`SELECT EXISTS(SELECT 1 FROM users WHERE email = $1);`, [email])).rows[0].exists;
-    }
-
-    async findOne(email){
-        return (await db.query(`SELECT * FROM users WHERE email = $1`, [email])).rows[0] 
-    }
-
-    async login(){
-
-    }
-    async create(name, email, password){
-       
-        return (await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, 
-            [name, email, password]
-            )).rows;
-        
-    }
-    async getAll(){
-        return (await db.query(`SELECT * FROM users`)).rows;
-    }
+  async create(name, email, password) {
+    return (
+      await db.query(
+        `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+        [name, email, password]
+      )
+    ).rows[0];
+  }
+  async getAll() {
+    return (await db.query(`SELECT * FROM users;`)).rows;
+  }
 }
 
 module.exports = new User();
-
-
