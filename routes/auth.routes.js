180f0244a2f734879f350d9dca46479f4c2d02c4
egor <grebnevegor2000@mail.ru>
2021-09-16 12:20:34 +0300

    backend routing update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,47 +1,51 @@
-const Router = require('express');
+const Router = require("express");
 
-const { check } = require('express-validator');
-const userController = require('../controllers/user.controller');
-
+const { check } = require("express-validator");
+const userController = require("../controllers/user.controller");
 
 const router = Router();
 
-router.post('/signup', [
-    check('nickname')
-        .exists()
-        .withMessage('nickname is required')
-        .isLength({ min: 3 })
-        .withMessage('wrong nickname length'),
+router.post(
+  "/auth/signup",
+  [
+    check("username")
+      .exists()
+      .withMessage("nickname is required")
+      .isLength({ min: 3 })
+      .withMessage("wrong nickname length"),
 
-    check('email')
-        .exists()
-        .withMessage('email is required')
-        .isEmail()
-        .normalizeEmail({gmail_remove_dots: true})
-        .withMessage('email is not valid'),
-    
-    check('password')
-        .exists()
-        .withMessage('password is required')
-        .isLength({min:6})
-        .withMessage('password shoul be at least 6 symbols')
-    ],
-    userController.createUser);
-router.post('/login', [
-    check('email')
-        .exists()
-        .withMessage('email is required')
-        .isEmail()
-        .normalizeEmail({gmail_remove_dots: true})
-        .withMessage('email is not valid'),
-    
-    check('password')
-        .exists()
-        .withMessage('password is required')
-        .isLength({min:6})
-        .withMessage('password shoul be at least 6 symbols')
-], userController.loginUser);
+    check("email")
+      .exists()
+      .withMessage("email is required")
+      .isEmail()
+      .normalizeEmail({ gmail_remove_dots: true })
+      .withMessage("email is not valid"),
+
+    check("password")
+      .exists()
+      .withMessage("password is required")
+      .isLength({ min: 6 })
+      .withMessage("password shoul be at least 6 symbols"),
+  ],
+  userController.createUser
+);
+router.post(
+  "/auth/login",
+  [
+    check("email")
+      .exists()
+      .withMessage("email is required")
+      .isEmail()
+      .normalizeEmail({ gmail_remove_dots: true })
+      .withMessage("email is not valid"),
+
+    check("password")
+      .exists()
+      .withMessage("password is required")
+      .isLength({ min: 6 })
+      .withMessage("password shoul be at least 6 symbols"),
+  ],
+  userController.loginUser
+);
 
 module.exports = router;
-
-
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,14 @@
-const {Router} = require('express');
+const { Router } = require("express");
 
-const userController = require('../controllers/user.controller');
+const userController = require("../controllers/user.controller");
 
 const router = Router();
 
 // router.post('/user/create', userController.createUser); // Maybe register??
 
-router.get('/user/get', userController.getUsers);
-router.get('/user/:id', userController.getOneUser);
-router.put('/user/update', userController.updateUser);
-router.delete('/user/delete/:id', userController.deleteUser);
+router.get("/user/get", userController.getUsers);
+router.get("/user/:id", userController.getOneUser);
+router.put("/user/update", userController.updateUser);
+router.delete("/user/delete/:id", userController.deleteUser);
 
 module.exports = router;
