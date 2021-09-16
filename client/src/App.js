2ed88c513818b6d6ef1c60db4181c16249014d61
egor <grebnevegor2000@mail.ru>
2021-09-16 12:23:54 +0300

    minor update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import './App.css'
-import {BrowserRouter as Router} from 'react-router-dom';
-import { AppRouter } from './router/AppRouter';
-import {useAuth} from './hooks/auth.hook';
-import {AuthContext} from './context/AuthContext';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRouter } from "./router/AppRouter";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const {login, logout, token, userId} = useAuth();
+  const { login, logout, token, userId } = useAuth();
   const isAuthenticated = !!token;
   const routing = AppRouter(isAuthenticated);
   return (
-      <AuthContext.Provider value={{
-        token, userId, login, logout, isAuthenticated
-      }}>
-        <Router>  
-          <div className="container">
-            {routing}
-          </div>
-        </Router>
-      </AuthContext.Provider>
+    <AuthContext.Provider
+      value={{ token, userId, login, logout, isAuthenticated }}
+    >
+      <Router>
+        <div className="container">{routing}</div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
