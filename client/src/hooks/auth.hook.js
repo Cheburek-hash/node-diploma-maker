e8be14f8cb1f57533caf4051105aff7ea79a561c
egor <grebnevegor2000@mail.ru>
2021-09-16 12:25:45 +0300

    formatting, minor update

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,37 +1,37 @@
 import { useState, useCallback, useEffect } from "react";
 
 export const useAuth = () => {
-    const userStorageName = 'u_data';
+  const userStorageName = "u_data";
 
-    const [token, setToken] = useState(null);
-    const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
 
-    const login = useCallback((jwtToken, id) => {
-        console.log('Loaded!');
-        setToken(jwtToken);
-        setUserId(id);
+  const login = useCallback((jwtToken, id) => {
+    console.log("Loaded!");
+    setToken(jwtToken);
+    setUserId(id);
 
-        localStorage.setItem(userStorageName, JSON.stringify({
-            userId: id,
-            token: jwtToken
-        }));
-    }, []);
+    localStorage.setItem(
+      userStorageName,
+      JSON.stringify({
+        userId: id,
+        token: jwtToken,
+      })
+    );
+  }, []);
 
-    const  logout = useCallback(() => {
-        setToken(null);
-        setUserId(null);
-        localStorage.removeItem(userStorageName);
-    }, []);
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    localStorage.removeItem(userStorageName);
+  }, []);
 
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem(userStorageName))
-        if (data && data.token){
-            login(data.token, data.userId)
-        }
-    }, [login])
-
-    return {
-        login, logout, token, userId
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem(userStorageName));
+    if (data && data.token) {
+      login(data.token, data.userId);
     }
+  }, [login]);
 
-}
+  return { login, logout, token, userId };
+};
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,23 +5,24 @@ import { Account } from "../pages/Account";
 import { About } from "../pages/About";
 
 export const ROUTES = {
-    HOME:         '/',
-    AUTH:         '/auth',
-    CONSTRUCTOR:  '/constructor',
-    ACCOUNT:      '/account',
-    ABOUT:        '/about'
-}
+  HOME: "/",
+  AUTH_LOGIN: "/auth/login",
+  AUTH_SIGNUP: "/auth/signup",
+  CONSTRUCTOR: "/constructor",
+  ACCOUNT: "/account",
+  ABOUT: "/about",
+};
 
 export const publicRoutes = [
-    {path: ROUTES.HOME, exact: true, component: Home},
-    {path: ROUTES.AUTH, exact: true, component: Auth},
-    {path: ROUTES.ABOUT, exact: true, component: About}
+  { path: ROUTES.HOME, exact: true, component: Home },
+  { path: ROUTES.AUTH_LOGIN, exact: true, component: Auth },
+  { path: ROUTES.AUTH_SIGNUP, exact: true, component: Auth },
+  { path: ROUTES.ABOUT, exact: true, component: About },
 ];
 
 export const privateRoutes = [
-    {path: ROUTES.HOME, exact: true, component: Home},
-    {path: ROUTES.CONSTRUCTOR, exact: true, component: Constructor},
-    {path: ROUTES.ACCOUNT, exact: true, component: Account},
-    {path: ROUTES.ABOUT, exact: true, component: About}
+  { path: ROUTES.HOME, exact: true, component: Home },
+  { path: ROUTES.CONSTRUCTOR, exact: true, component: Constructor },
+  { path: ROUTES.ACCOUNT, exact: true, component: Account },
+  { path: ROUTES.ABOUT, exact: true, component: About },
 ];
-
