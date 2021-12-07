ffdb7cbc3bc5c7247f3ad7847c375c24e676ea1a
egor <grebnevegor2000@mail.ru>
2021-12-07 11:29:23 +0300

    show local ip

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const userRouter = require("../routes/user.routes");
 const authRouter = require("../routes/auth.routes");
 const cors = require("cors");
+const os = require("os");
 
 class Server {
   constructor() {
@@ -13,9 +14,12 @@ class Server {
     this.ROOT = require("path").dirname(require.main.filename);
     this.app = express();
   }
-  start = () => {
+  getLocalIp() {
+    return Object.values(os.networkInterfaces()).reduce((r, list) =>r.concat(list.reduce((rr, i) => rr.concat((i.family === "IPv4" && !i.internal && i.address) || []),[])),[])[1];
+  }
+  start() {
     this.app.listen(this.PORT, () => {
-      console.log(`App listening at :${this.PORT}`);
+      console.log(`App listening at ${this.getLocalIp()}:${this.PORT}`);
     });
     this.app.use(bodyParser.json());
     this.app.use(cors());
@@ -27,10 +31,10 @@ class Server {
         express.static(path.join(this.ROOT, "client", "build"))
       );
       this.app.get("*", (req, res) => {
-        res.sendFile(path.resolve(this.ROOT,"client", "build", "index.html"));
+        res.sendFile(path.resolve(this.ROOT, "client", "build", "index.html"));
       });
     }
-  };
+  }
 }
 
 module.exports = Server;
