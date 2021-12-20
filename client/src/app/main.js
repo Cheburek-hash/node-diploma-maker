9a06cfd3e14430a6f05a4de26c17637ce953ebc5
egor <grebnevegor2000@mail.ru>
2021-12-20 14:44:48 +0300

    text functions

--- client/src/app/main.js
+++ client/src/app/main.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import * as PIXI from "pixi.js";
-import { Stage, Container, render, unmountComponentAtNode  } from "@inlet/react-pixi";
-import { Bunny, DraggableImage, DraggableObject } from "./graphics/source";
+import {
+  Stage,
+  Container,
+  render,
+  unmountComponentAtNode,
+} from "@inlet/react-pixi";
+import {
+  Bunny,
+  DraggableImage,
+  DraggableObject,
+  DraggableText,
+} from "./graphics/source";
+import { TextStyle } from "pixi.js";
 
 PIXI.utils.skipHello();
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
@@ -15,9 +26,25 @@ export class Canvas extends React.Component {
     this.h = 1054 * this.pixelRatio;
     this.c = 0x01262a;
     this.stage = React.createRef();
-    this.components = [];
     this.handleClick = this.handleClick.bind(this);
-    this.componentsBuffer = []
+    this.componentsBuffer = [];
+    this.TextStyle = new TextStyle({
+      align: "center",
+      fontFamily: '"Source Sans Pro", Helvetica, sans-serif',
+      fontSize: 50,
+      fontWeight: 400,
+      fill: ["#ffffff", "#00ff99"], // gradient
+      stroke: "#01d27e",
+      strokeThickness: 5,
+      letterSpacing: 20,
+      dropShadow: true,
+      dropShadowColor: "#ccced2",
+      dropShadowBlur: 4,
+      dropShadowAngle: Math.PI / 6,
+      dropShadowDistance: 6,
+      wordWrap: true,
+      wordWrapWidth: 440,
+    });
   }
 
   toDataUrl() {
@@ -28,61 +55,68 @@ export class Canvas extends React.Component {
   changeTheme() {
     console.log(this.stage.current);
   }
-  // removeComponent(){
-  //   unmountComponentAtNode(this.stage.current.app.stage);
-  // }
-  addComponent(type) {
-
-    switch (type) {
+  removeComponent() {
+    unmountComponentAtNode(this.stage.current.app.stage.children.pop());
+  }
+  addComponent(info) {
+    switch (info.type) {
       case "text":
+        this.componentsBuffer.push(
+          <Container key={uuidv4()}>
+            <DraggableText
+              text={info?.data}
+             
+              height={100}
+              style={this.TextStyle}
+              x={150}
+              y={100}
+            />
+          </Container>
+        );
         break;
-        case "square":
-          // this.componentsBuffer.push(<DraggableObject key={uuidv4()} tint={0x00ffff} x={150} y={100} />)
-          // console.log(this.componentsBuffer);
-         
-          render(<DraggableObject key={uuidv4()} tint={0x00ffff} x={150} y={100} />, this.stage.current.app.stage);
-         
-          
-          break;
-      default:
+      case "square":
+        this.componentsBuffer.push(
+          <Container key={uuidv4()}>
+            <DraggableObject
+              width={100}
+              height={100}
+              tint={0x00ffff}
+              x={300}
+              y={100}
+            />
+          </Container>
+        );
+
         break;
     }
+    render(
+      <React.Fragment>{this.componentsBuffer}</React.Fragment>,
+      this.stage.current.app.stage
+    );
   }
 
   handleClick(event) {
-    // this.components.push({comp: <DraggableObject tint={0x00ffff} x={150} y={100} />})
     console.log("click!");
-    
-    
   }
 
   render() {
     return (
-      
-        <Stage
-          width={this.w}
-          height={this.h}
-          options={{ autoDensity: true, backgroundColor: this.c }}
-          ref={this.stage}
-          onClick={this.handleClick}
-        >
-          <Container>
-            {/* <DraggableObject tint={0x00ffff} x={150} y={100} /> */}
-            {/* <DraggableImage
+      <Stage
+        width={this.w}
+        height={this.h}
+        options={{ autoDensity: true, backgroundColor: this.c }}
+        ref={this.stage}
+        onClick={this.handleClick}
+      >
+        <Container>
+          {/* <DraggableObject tint={0x00faaf} x={150} y={100} /> */}
+
+          {/* <DraggableImage
           source="https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png"
           key={uuidv4()}
         /> */}
-        
-           {this.components.forEach(obj => {
-            <obj.comp key={uuidv4()} />
-          })
-          } 
-           
-            
-          
-          </Container>
-        </Stage>
-     
+        </Container>
+      </Stage>
     );
   }
 }
